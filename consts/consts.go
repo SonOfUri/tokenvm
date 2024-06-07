04f76b759b525257094e58d31c5a0c3fca639a61
Alo Timothy <alotimothy01@gmail.com>
2024-06-07 14:57:39 +0100

    updates

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Alo"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Timothy"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "alo"
 )
 
 var ID ids.ID
